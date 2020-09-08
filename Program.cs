bf8c5a261d0894fbd9f3071336eed8f717c6a40d
Nompilo Moyo <Nompilo.Moyo@Conlog.com>
2020-09-08 23:42:35 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+
+namespace LinqSamples
+{
+    class Program
+    {
+        //this program returns files in memory according to the file size and uses an interface to compare files and list them from largest to smallest
+        static void Main(string[] args)
+        {
+            string path = @"C:\windows";
+            ShowLargeFilesWithoutLinq(path);
+            ShowLargeFilesWithLinq(path);
+
+        }
+        private static void ShowLargeFilesWithLinq(string path)
+        {
+            var query = from file in new DirectoryInfo(path).GetFiles()
+                        orderby file.Length descending
+                        select file;
+
+            foreach(var file in query.Take(5))
+            {
+                Console.WriteLine($"{file.Name,-20} : {file.Length,10:N0}");
+            }
+        }
+
+        private static void ShowLargeFilesWithoutLinq(string path)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+            FileInfo[] files = directory.GetFiles();
+            Array.Sort(files, new FileInfoComparer());
+
+            /**foreach(FileInfo file in files)
+            {
+                Console.WriteLine($"{file.Name} : {file.Length}");
+            }**/
+
+            for (int i=0; i<5;i++)
+            {
+                FileInfo file = files[i];
+                Console.WriteLine($"{file.Name, -20} : {file.Length, 10:N0}");
+            }
+        }
+    }
+    public class FileInfoComparer : IComparer<FileInfo>
+    {
+        public int Compare(FileInfo x, FileInfo y)
+        {
+            return y.Length.CompareTo(x.Length);
+        }
+    }
+}
